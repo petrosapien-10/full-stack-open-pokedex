fe431fa002fc0be556dc08e2c116861e08a3c9a6
petrosapien-10 <nhutlongnguyen.work@gmail.com>
2025-10-03 17:21:25 +0300

    Add automatic deployment to Fly.io in CI/CD pipeline

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const PORT = process.env.PORT || 3001
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) // eslint-disable-line no-console
 })
