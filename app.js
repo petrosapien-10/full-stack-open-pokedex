778c3b9df6635a8be302a02d3f4e54085d0430ec
petrosapien-10 <nhutlongnguyen.work@gmail.com>
2025-10-04 04:42:39 +0300

    #skip, this should skip deploy and tags

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001
 
 app.use(express.static('dist'))
 app.get('/version', (req, res) => {
-  res.send('skip work') // change this string to ensure a new version deployed
+  res.send('if this show, skip is not working') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
